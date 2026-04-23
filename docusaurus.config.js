154e056b605ee66d623b03388a8b820a4fec8e32
adden WZK <3142364740@qq.com>
2026-04-23 21:58:17 +0800

    docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,16 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://zachary-wu99.github.io/iFly_Ctrl/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'zachary-wu99', // Usually your GitHub org/user name.
+  projectName: 'iFly_Ctrl', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
