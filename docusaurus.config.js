ebe10ca810b32293a9617385353300c16de7c0a1
adden WZK <3142364740@qq.com>
2026-04-23 22:04:54 +0800

    docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://zachary-wu99.github.io/iFly_Ctrl/',
+  url: 'https://zachary-wu99.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/iFly_Ctrl/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
